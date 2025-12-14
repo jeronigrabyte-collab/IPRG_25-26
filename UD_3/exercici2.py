f9e79c70276db92e682ad6fdcbc94cd0c361331c
jeroni <jeronigrabyte@gmail.com>
2025-12-14 22:44:54 +0100

    3.2

--- UD_3/exercici2.py
+++ UD_3/exercici2.py
@@ -1,37 +1,25 @@
-fitxer1 = input("Introdueix el nom del primer fitxer: ")
-fitxer2 = input("Introdueix el nom del segon fitxer: ")
-fitxer3 = input("Introdueix el nom del tercer fitxer: ")
+def llegir_fitxer(nom_fitxer):
+    try:
+        with open(nom_fitxer, "r") as f:
+            return f.read() + " "
+    except FileNotFoundError:
+        print(f"Error: El fitxer {nom_fitxer} no existeix")
+        return ""
 
-fitxer_final = input("Introdueix el nom del fitxer final: ")
+def concatenar_fitxers():
+    fitxer1 = input("Nom del primer fitxer: ")
+    fitxer2 = input("Nom del segon fitxer: ")
+    fitxer3 = input("Nom del tercer fitxer: ")
+    fitxer_final = input("Nom del fitxer final: ")
+    
+    contingut = ""
+    contingut += llegir_fitxer(fitxer1)
+    contingut += llegir_fitxer(fitxer2)
+    contingut += llegir_fitxer(fitxer3)
+    
+    with open(fitxer_final, "w") as f:
+        f.write(contingut.strip())
+    
+    print(f"\nFitxers concatenats en {fitxer_final}")
 
-contingut_total = ""
-
-try:
-    f1 = open(fitxer1, "r")
-    contingut_total = contingut_total + f1.read() + " "
-    f1.close()
-    print("Fitxer 1 llegit correctament")
-except:
-    print("Error: El fitxer " + fitxer1 + " no existeix")
-
-try:
-    f2 = open(fitxer2, "r")
-    contingut_total = contingut_total + f2.read() + " "
-    f2.close()
-    print("Fitxer 2 llegit correctament")
-except:
-    print("Error: El fitxer " + fitxer2 + " no existeix")
-
-try:
-    f3 = open(fitxer3, "r")
-    contingut_total = contingut_total + f3.read() + " "
-    f3.close()
-    print("Fitxer 3 llegit correctament")
-except:
-    print("Error: El fitxer " + fitxer3 + " no existeix")
-
-fitxer_nou = open(fitxer_final, "w")
-fitxer_nou.write(contingut_total)
-fitxer_nou.close()
-
-print("\nFitxers concatenats correctament en " + fitxer_final)
+concatenar_fitxers()
